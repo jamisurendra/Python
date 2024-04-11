fd97a6048364745969f0cf42223106b179ae9edb
Surendra Jami <80164742+jamisurendra@users.noreply.github.com>
2024-04-11 10:36:22 +0530

    Update Interview.py

--- Interview.py
+++ Interview.py
@@ -13,3 +13,28 @@ def add_num(l):
 
 l = [1,2]
 print(add_num(l))
+
+#### Adding 2 Numbers :
+
+def add_numbers(x,y):
+    x = x**2
+    y = y**3
+    return x + y
+
+result= add_numbers(3,3)
+print(result)
+
+##  Print the values 
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+
+### Display 5 table 
+count = 0
+
+while count < 121:
+    print(count)
+    count += 5
+
+
